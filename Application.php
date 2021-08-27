b662b557f19e0e0269762f91486b07fb7ea1410f
Mosaab Ehab <musab.ehab@gmail.com>
2021-08-28 00:05:01 +0200

    Update Application.php

--- Application.php
+++ Application.php
@@ -7,6 +7,11 @@ use Exception;
 
 class Application
 {
+    const EVENT_BEFORE_REQUEST = 'beforeRequest';
+    const EVENT_AFTER_REQUEST = 'afterRequest';
+
+    protected array $eventListeners = [];
+
     public static string $ROOT_DIR;
     public static Application $app;
     public string $layout = 'main';
@@ -50,6 +55,8 @@ class Application
 
     public function run()
     {
+        $this->triggerEvent(self::EVENT_BEFORE_REQUEST);
+
         try {
             echo $this->router->resolve();
         } catch (Exception $e) {
@@ -76,5 +83,20 @@ class Application
     {
         $this->user = null;
         $this->session->remove('user');
+    }
+
+    public function triggerEvent($eventName)
+    {
+        $callbacks = $this->eventListeners[$eventName] ?? [];
+
+        foreach ($callbacks as $callback){
+
+            call_user_func($callback);
+        }
+    }
+
+    public function on($eventName, $callback)
+    {
+        $this->eventListeners[$eventName][] = $callback;
     }
 }
